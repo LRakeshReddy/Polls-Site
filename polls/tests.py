0242f6a0c0f16a79aeaff6e4125d73be0f0ed723
LRakeshReddy <lrr2567@outlook.com>
2020-09-15 23:47:49 +0530

    commit to 'master'

--- polls/tests.py
+++ polls/tests.py
@@ -37,7 +37,7 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            ['Question: Past question.>']
+            ['<Question: Past Question.>']
         )
 
     def test_future_question(self):
@@ -61,18 +61,18 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+            ['<Question: Past question 1.>', '<Question: Past question 2.>']
         )
 
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
-        future_question=create_question(question_text='Future question. ', days = 5)
-        url = reverse('polls:details', args=(future_question.id,))
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
-    def test_path_question(self):
-        past_question=create_question(question_text='Past Question. ', days=5)
-        url=reverse('polls:detail', args=(past_question.id,))
+    def test_past_question(self):
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
